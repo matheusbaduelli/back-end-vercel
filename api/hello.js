7150fd14a7518aee43ca7ea9095370926efd7ee4
Matheus Rodrigues de Lima <mhs.djc@gmail.com>
2025-05-04 10:53:55 -0300

    first commit

---
+++ api/hello.js
@@ -0,0 +1,14 @@
+// api/hello.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
+module.exports = app; // Importante para Vercel
